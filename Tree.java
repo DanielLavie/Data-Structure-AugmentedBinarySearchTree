243ec7436b46a9e5d21fe6388fb9f81a1e7bfbda
DanielLavie <daniel.lavie.93@gmail.com>
2016-06-12 01:02:14 +0300

    Lay out Tree class. Implemented find method.

--- Tree.java
+++ Tree.java
@@ -2,4 +2,46 @@
  * This class represents an augmented binary search tree
  */
 public class Tree {
+
+    private TreeNode root;
+
+    /**
+     * Construct an empty tree
+     */
+    public Tree() {
+        this.root = null;
+    }
+
+    /**
+     * Find the node in the tree with the given key
+     * @param keyToLookUp   The key to look up in the tree
+     * @return              A reference to the node with the specific key
+     *                      if such node exists, null otherwise
+     */
+    public TreeNode find(double keyToLookUp) {
+
+        if (isEmpty()) {
+            return null;
+        }
+
+        TreeNode currentNode = root;
+        while (currentNode != null &&
+                currentNode.getKey() != keyToLookUp) {
+            if (currentNode.getKey() > keyToLookUp) {
+                currentNode = currentNode.getLeft();
+            }
+            else {
+                currentNode = currentNode.getRight();
+            }
+        }
+
+        return currentNode;
+    }
+
+    /**
+     * @return True if the tree is empty (has no nodes in it)
+     */
+    private boolean isEmpty() {
+        return root == null;
+    }
 }
