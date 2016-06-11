7619b7db70d553caa0169a3f52b4e07a2b8a636e
DanielLavie <daniel.lavie.93@gmail.com>
2016-06-12 00:32:52 +0300

    Implemented TreeNode class as given in the exercise. No need for comment as this has a very trivial structure

--- TreeNode.java
+++ TreeNode.java
@@ -1,5 +1,66 @@
 /**
  * This class represents a node in augmented binary search tree
  */
-public class TreeNode {
+class TreeNode {
+
+    private double key;
+
+    private TreeNode left;
+    private TreeNode right;
+    private TreeNode parent;
+
+    private int size;
+    private int height;
+
+    TreeNode(double key) {
+        this.key = key;
+    }
+
+    public double getKey() {
+        return key;
+    }
+
+    public void setKey(double key) {
+        this.key = key;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
+
+    public TreeNode getParent() {
+        return parent;
+    }
+
+    public void setParent(TreeNode parent) {
+        this.parent = parent;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
 }
