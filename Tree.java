79d6ae3a1ad9b6c4f6f5c7d5ab2fd5545f0aa943
Daniel Lavie <lavieda@netvision.net.il>
2016-06-12 20:20:50 +0300

    Implemented insert and find methods.

--- Tree.java
+++ Tree.java
@@ -1,3 +1,5 @@
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 /**
  * This class represents an augmented binary search tree
  */
@@ -25,7 +27,7 @@ public class Tree {
         }
 
         TreeNode currentNode = root;
-        while (currentNode != null &&
+        while (null != currentNode &&
                 currentNode.getKey() != keyToLookUp) {
             if (currentNode.getKey() > keyToLookUp) {
                 currentNode = currentNode.getLeft();
@@ -38,9 +40,108 @@ public class Tree {
     }
 
     /**
-     * @return True if the tree is empty (has no nodes in it)
+     * Insert a given node in it's proper position in the tree, according to it's key
+     * @param newNode A reference to the node to be inserted into this tree
+     */
+    public void insert(TreeNode newNode) {
+
+        if (null == newNode) {
+            throw new RuntimeException("Please insert a valid tree node");
+        }
+
+        if (isEmpty()) {
+            root = newNode;
+            return;
+        }
+
+        // Find the correct place to insert the mew node
+
+        TreeNode newNodeParent = null;
+        TreeNode nodeIterator = root;
+
+        while (null != nodeIterator) {
+            newNodeParent = nodeIterator;
+            if (newNode.getKey() < nodeIterator.getKey()) {
+                nodeIterator = nodeIterator.getLeft();
+            }
+            else {
+                nodeIterator = nodeIterator.getRight();
+            }
+        }
+
+        // Insert the new node to it's correct place
+
+        newNode.setParent(newNodeParent);
+        if (newNode.getKey() < newNodeParent.getKey()) {
+            newNodeParent.setLeft(newNode);
+        }
+        else {
+            newNodeParent.setRight(newNode);
+        }
+
+        // Reset new node members
+        newNode.setHeight(0);
+        newNode.setSize(1);
+
+        // Update size and height for the path of the new leaf
+        nodeIterator = newNodeParent;
+        int currentHeight = 1;
+        while (null != nodeIterator) {
+            nodeIterator.setSize(nodeIterator.getSize() + 1);
+
+            if (nodeIterator.getHeight() < currentHeight) {
+                nodeIterator.setHeight(currentHeight);
+            }
+
+            nodeIterator = nodeIterator.getParent();
+        }
+    }
+
+    /**
+     * Delete a node from this tree.
+     * This function assumes that a node with the same key exists in the tree.
+     * @param deleteNode The node to be deleted.
+     */
+    void delete (TreeNode deleteNode) {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * Retrieve the node with the given rank in the tree.
+     * rank 1 means the smallest node in the tree.
+     * @param requiredRank The rank to be found in the tree
+     * @return A reference to the node with the given rank
+     */
+    TreeNode OSSelect (int requiredRank) {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * Retrieve the rank of the given tree node.
+     * This function assumes that a node with the same key exists in the tree.
+     * @param requiredNode The node to be searched for it's rank
+     * @return The rank of the given node
+     */
+    int OSRank (TreeNode requiredNode) {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * Check if a sub-tree of the this tree is balanced.
+     * A balanced tree means - the height of the sub-tree root is less than or
+     * equals to 2logN, when N is the number of nodes in the subtree.
+     *
+     * @param node The root of the sub tree to be checked for balance
+     * @return True if the sub tree is balanced, false otherwise
+     */
+    boolean isBalanced (TreeNode node) {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * @return True if the tree is empty (has no nodes in it), false otherwise
      */
     private boolean isEmpty() {
-        return root == null;
+        return null == root;
     }
 }
